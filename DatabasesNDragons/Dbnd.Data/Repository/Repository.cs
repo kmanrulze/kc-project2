c3b5846cb2b56f6030c6491222d5cff7e73d06ba
kmanrulze <kcguzman@comcast.net>
2019-10-28 11:02:26 -0500

    Repository logic made with dependency injection

--- DatabasesNDragons/Dbnd.Data/Repository/Repository.cs
+++ DatabasesNDragons/Dbnd.Data/Repository/Repository.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Dbnd.Logic.Interfaces;
 
 namespace Dbnd.Data.Repository
 {
-    class Repository
+    class Repository : IRepository
     {
-
+        private readonly ApplicationContext _context;
+        public Repository(ApplicationContext context)
+        {
+            _context = context;
+        }
     }
 }
--- DatabasesNDragons/Dbnd.Logic/Interfaces/IRepository.cs
+++ DatabasesNDragons/Dbnd.Logic/Interfaces/IRepository.cs
@@ -4,7 +4,8 @@ using System.Text;
 
 namespace Dbnd.Logic.Interfaces
 {
-    interface IRepository
+    public interface IRepository
     {
+
     }
 }
