da6973b60803bd51955b58a25911d9baf76c00c4
RAINBOOMERS-PC <kcguzman@comcast.net>
2019-10-29 18:27:16 -0500

    Small fix: No need for e to be the name of an error unless writing out the message in the catch.

--- DatabasesNDragons/Dbnd.Logic/Objects/Client.cs
+++ DatabasesNDragons/Dbnd.Logic/Objects/Client.cs
@@ -79,11 +79,11 @@ namespace Dbnd.Logic.Objects
                     return match.Groups[1].Value + domainName;
                 }
             }
-            catch (RegexMatchTimeoutException e)
+            catch (RegexMatchTimeoutException)
             {
                 return false;
             }
-            catch (ArgumentException e)
+            catch (ArgumentException)
             {
                 return false;
             }
