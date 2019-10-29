9b3b59fa087fe803c783d0f360d7c899488bdb72
kmanrulze <kcguzman@comcast.net>
2019-10-29 10:31:23 -0500

    Added tests

---
+++ DatabasesNDragons/Dbnd.Test/DungeonMasterTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Dbnd.Logic.Objects;
+
+namespace Dbnd.Test
+{
+    public class DungeonMasterTests
+    {
+        [Fact]
+        public void CheckAddGame()
+        {
+            DungeonMaster testDM = new DungeonMaster();
+            testDM.Games.Add(new Game());
+            Assert.True(testDM.Games.Count == 1);
+        }
+    }
+}
