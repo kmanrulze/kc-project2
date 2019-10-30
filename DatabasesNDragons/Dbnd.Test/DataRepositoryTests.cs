63b772a398027db2d93ed7993eae3827a5082b55
Shawn <43576702+saclark12000@users.noreply.github.com>
2019-10-29 20:03:19 -0500

    added moq to test project, began moq work with leaving example for my future self in test project

---
+++ DatabasesNDragons/Dbnd.Test/DataRepositoryTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dbnd.Data.Repository;
+using Xunit;
+using Moq;
+
+namespace Dbnd.Test
+{
+    class DataRepositoryTests
+    {
+        // Example Usage of Moq data to Unit test repository use
+        //[Fact]
+        //public void MethodToTestFromAControllerNotNull()
+        //{
+        //    var listOfGames = new List<Dbnd.Logic.Objects.Game>();
+        //    listOfGames.Add(new Dbnd.Logic.Objects.Game() 
+        //    { 
+        //        GameName = "EyeOfTheBeHolder",
+        //        DungeonMasterID = Guid.NewGuid()
+        //    });
+        //    listOfGames.Add(new Dbnd.Logic.Objects.Game()
+        //    {
+        //        GameName = "NeverwinterNights",
+        //        DungeonMasterID = Guid.NewGuid()
+        //    });
+            //    Mock<Repository> mockRepository = new Mock<Repository>();
+            //    mockRepository.Setup(x => x.GetAllGamesByDungeonMasterID(Guid.NewGuid())).Returns(listOfGames);
+            //    var someController = new SomeController(mockRepository.Object);
+            //    var test = bandController.MethodToTest();
+            //    Assert.NotNull(test);
+            //    Assert.Equals(expected, outcome); etc...
+        //}
+    }
+}
