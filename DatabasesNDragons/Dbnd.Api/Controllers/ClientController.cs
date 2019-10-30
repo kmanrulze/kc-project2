86138b35ec4d624e03a075bb3db3f817153ad24d
kmanrulze <kcguzman@comcast.net>
2019-10-30 12:46:19 -0500

    Added repo to API. Not sure if proper practice, but that's the thing for now

--- DatabasesNDragons/Dbnd.Api/Controllers/ClientController.cs
+++ DatabasesNDragons/Dbnd.Api/Controllers/ClientController.cs
@@ -2,20 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Dbnd.Logic.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Dbnd.Api.Controllers
 {
+
     [Route("api/[controller]")]
     [ApiController]
     public class ClientController : ControllerBase
     {
+        private readonly IRepository _repository;
+        public ClientController(IRepository repository)
+        {
+            _repository = repository;
+        }
         // GET: api/Client
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
+
         }
 
         // GET: api/Client/5
