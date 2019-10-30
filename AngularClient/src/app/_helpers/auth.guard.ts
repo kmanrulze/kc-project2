47abde58640b2d0efda2265b1e736141d5b8e0ac
Jacob Davis <jacob.marquez.davis@hotmail.com>
2019-10-30 12:58:36 -0500

    Trying to push the AngularClient folder.

---
+++ AngularClient/src/app/_helpers/auth.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot }
+    from '@angular/router';
+
+import { AuthenticationService } from '../_services/authentication.service';
+import { Statement } from '@angular/compiler';
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate
+{
+    constructor(private router: Router,
+        private authenticationService: AuthenticationService)
+        {
+
+        }
+
+    canActivate(route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot)
+    {
+        const currentUser = this.authenticationService.currentUserValue;
+        if (currentUser)
+        {
+            return true;
+        }
+
+        this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
+    }
+}
