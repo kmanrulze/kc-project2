503d611e996c138d83f4092d231c7a8e9eac8251
kmanrulze <kcguzman@comcast.net>
2019-11-01 13:40:14 -0500

    ASPNETCore.App package installed to tests to start testing API. ClientAPITest class added and things arranged within folders

---
+++ DatabasesNDragons/Dbnd.Test/API Tests/ClientAPITests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dbnd.Api.Controllers;
+using Xunit;
+
+namespace Dbnd.Test.API_Tests
+{
+    public class ClientAPITests
+    {
+        [Fact]
+        public void TestGetAllClients()
+        {
+            throw new NotImplementedException();
+        }
+        [Fact]
+        public void TestGetCertainClient()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
