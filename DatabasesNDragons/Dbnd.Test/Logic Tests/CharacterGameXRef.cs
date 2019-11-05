44e58c21eb607d7b0537a9cea932098d0e24dfd1
Shawn <43576702+saclark12000@users.noreply.github.com>
2019-11-04 21:20:16 -0600

    unit test for chargamexref

---
+++ DatabasesNDragons/Dbnd.Test/Logic Tests/CharacterGameXRef.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xunit;
+using Dbnd.Logic.Objects;
+
+namespace Dbnd.Test.Logic_Tests
+{
+    
+    public class CharacterGameXRef
+    {
+        [Fact]
+        public void TestIDForGUID()
+        {
+            var gameID = Guid.NewGuid();
+            var chacacterID = Guid.NewGuid();
+            Data.Entities.CharacterGameXRef testXRef = new Data.Entities.CharacterGameXRef()
+            {
+                CharacterID = chacacterID,
+                GameID = gameID
+            };
+            Guid guidResult;
+            Assert.True(Guid.TryParse(testXRef.GameID.ToString(), out guidResult));
+            Assert.True(Guid.TryParse(testXRef.CharacterID.ToString(), out guidResult));
+
+        }
+    }
+}
