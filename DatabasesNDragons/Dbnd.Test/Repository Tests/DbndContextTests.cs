a645c2c7ba421104d1918c6eb251c9bf2a7f236d
Shawn <43576702+saclark12000@users.noreply.github.com>
2019-11-06 00:33:02 -0600

    cleaned up test for userprofile, test efcore.useinmemorydatabase unit testing

---
+++ DatabasesNDragons/Dbnd.Test/Repository Tests/DbndContextTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+using Dbnd.Data.Entities;
+using Dbnd.Data.Repository;
+
+namespace Dbnd.Test.Repository_Tests
+{
+    public class DbndContextTests
+    {
+        [Fact]
+        public void DbnDContextTest()
+        {
+            var options = new DbContextOptionsBuilder<DbndContext>()
+            .UseInMemoryDatabase(databaseName: "Dbnd")
+            .Options;
+
+            using (var context = new DbndContext(options))
+            {
+                context.Client.Add(new Client { UserName = "Aramand", Email = "Aramand@gmail.com" });
+                context.Client.Add(new Client { UserName = "Lestat", Email = "Lestat@gmail.com" });
+                context.Client.Add(new Client { UserName = "Claudia", Email = "Claudia@gmail.com" });
+                context.SaveChanges();
+            }
+
+            using (var context = new DbndContext(options))
+            {
+                Repository repository = new Repository(context);
+                var clientbyEmail = repository.GetClientByEmailAsync("Aramand@gmail.com").Result;
+                var clientbyID = repository.GetClientByIDAsync(clientbyEmail.ClientID).Result;
+
+                Assert.Equal("Aramand", clientbyEmail.UserName);
+                Assert.Equal("Aramand", clientbyID.UserName);
+            }
+        }
+
+    }
+}
