fe217283b3265124095b82e992e45e64bbaeacc7
kmanrulze <kcguzman@comcast.net>
2019-11-08 16:31:39 -0600

    got proof of concept 5eAPI working

--- AngularClient/src/app/_services/DnDApi/rest.service.ts
+++ AngularClient/src/app/_services/DnDApi/rest.service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http
 import { Observable, of } from 'rxjs';
 import { map, catchError, tap } from 'rxjs/operators';
 
-const endpoint = 'http://dnd5eapi.co/api/';
+const endpoint = 'http://www.dnd5eapi.co/api/';
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type':  'application/json'
