fbf2d824f8e21f29ef80c6af99444ff556dd8d43
Shawn <43576702+saclark12000@users.noreply.github.com>
2019-11-09 20:57:36 -0600

    cleaned up boolean variables

--- AngularClient/src/app/playgame/overviewoptions/overviewoptions.component.ts
+++ AngularClient/src/app/playgame/overviewoptions/overviewoptions.component.ts
@@ -11,6 +11,7 @@ import { Observable } from 'rxjs';
 export class OverviewoptionsComponent implements OnInit {
   dbndProfText = '';
   showSpinner = true;
+  characterSelected = false;
   constructor(public auth: AuthService, public dbnd: DbndService) { }
 
   async ngOnInit() {
