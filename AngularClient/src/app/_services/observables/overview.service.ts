90d8e06f4ebebb10c97eaf6ca3dfb4892e982e31
RAINBOOMERS-PC <kcguzman@comcast.net>
2019-11-11 22:41:35 -0600

    Overviews made as casted observable

--- AngularClient/src/app/_services/observables/overview.service.ts
+++ AngularClient/src/app/_services/observables/overview.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { DbndService } from '../dbnd/dbnd.service';
 import { AuthService } from '../auth/auth.service';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { Game } from 'src/app/_models/game';
+import { Overview } from 'src/app/_models/overview';
 
 @Injectable({
   providedIn: 'root'
@@ -13,12 +13,13 @@ export class OverviewService {
     this.updateOverviews();
   }
 
-  private _overviews: BehaviorSubject<Game[]> = new BehaviorSubject([]);
-  public readonly overviews$: Observable<Game[]> = this._overviews.asObservable();
+  private _overviews: BehaviorSubject<Overview[]> = new BehaviorSubject([]);
+  public readonly overviews$: Observable<Overview[]> = this._overviews.asObservable();
 
   public async updateOverviews() {
     this.dbnd.getGameOverviews$(await this.auth.getClientId(), 'gameId')
-    .subscribe( res => {this._overviews.next(res as Overview[])});
+    .subscribe( res => {this._overviews.next(res as Overview[]);
+    });
   }
 
 }
