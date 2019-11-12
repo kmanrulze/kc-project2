2ba7bd7b5ca3a07ae30d8718585018ecf6bda1a7
kmanrulze <kcguzman@comcast.net>
2019-11-11 23:18:55 -0600

    service modified

--- AngularClient/src/app/_services/observables/overview.service.ts
+++ AngularClient/src/app/_services/observables/overview.service.ts
@@ -9,7 +9,7 @@ import { Overview } from 'src/app/_models/overview';
 })
 export class OverviewService {
 
-  constructor(private dbnd: DbndService, private auth: AuthService) {
+  constructor(private dbnd: DbndService, private auth: AuthService, public gameId: string) {
     this.updateOverviews();
   }
 
@@ -17,9 +17,8 @@ export class OverviewService {
   public readonly overviews$: Observable<Overview[]> = this._overviews.asObservable();
 
   public async updateOverviews() {
-    this.dbnd.getGameOverviews$(await this.auth.getClientId(), 'gameId')
+    this.dbnd.getGameOverviews$(await this.auth.getClientId(), this.gameId)
     .subscribe( res => {this._overviews.next(res as Overview[]);
     });
   }
-
 }
