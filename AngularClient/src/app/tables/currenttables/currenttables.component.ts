bb170d683b00122a1786a0b57b888cf6e309a998
RAINBOOMERS-PC <kcguzman@comcast.net>
2019-11-11 22:57:43 -0600

    gameId added to game constructor

--- AngularClient/src/app/tables/currenttables/currenttables.component.ts
+++ AngularClient/src/app/tables/currenttables/currenttables.component.ts
@@ -13,14 +13,19 @@ export class CurrenttablesComponent implements OnInit {
   dbndProfText = '';
   showSpinner = true;
   currentId: string;
+  games: any = [];
   constructor( public gameService: GameService, public auth: AuthService ) { }
 
   async ngOnInit() {
 
     await this.auth.getClientId().then( res => this.currentId = res);
+    this.getGames();
 
+  }
+  getGames() {
     this.gameService.games$.subscribe( async res => {
       this.showSpinner = false;
+      this.games = res;
     });
   }
 }
